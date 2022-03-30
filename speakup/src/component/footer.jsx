71d5be863ac237ae8fd8d1d2a03677228cd625ae
alfian_ezza <rianezza86@gmail.com>
2022-03-30 16:00:59 +0700

    Update Footer

--- speakup/src/component/footer.jsx
+++ speakup/src/component/footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faInstagram, faYoutube, faFacebookSquare} from '@fortawesome/free-brands-svg-icons';
-
 import "./footer.css";
 
 const Footer = () => {
@@ -13,46 +12,46 @@ const Footer = () => {
               <h4 id="footer-app-title">SpeakUp</h4>
               <p id="footer-app-subtitle">Counseling App</p>
               <p id="footer-app-desc">We will help you to overcome mental health problems and help you to reach your goals to make your life more meaningful</p>
-              <FontAwesomeIcon icon={faInstagram} id="ig"/>
-              <FontAwesomeIcon icon={faYoutube} className="offset-1" id="yt"/>
+              <a href="https://www.instagram.com/rianezza012/"><FontAwesomeIcon icon={faInstagram} id="ig"/></a>
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><FontAwesomeIcon icon={faYoutube} className="offset-1" id="yt"/></a>
               <FontAwesomeIcon icon={faFacebookSquare} className="offset-1" id="fb"/>
             </div>
-            <div className="col-1 offset-2">
+            <div id="company-this" className="col-1">
             <h4 id="company">Company</h4>
               <ul>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">Home</a>
+                <a href="#">Home</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">Feature</a>
+                <a href="#">Feature</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">Diary</a>
+                <a href="#">Diary</a>
               </li>
               </ul>
             </div>
-            <div className="col-1">
+            <div id="policy-this" className="col-1">
               <h4 id="policy">Policies</h4>
               <ul>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">Terms</a>
+                <a href="#">Terms</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">Privacy</a>
+                <a href="#">Privacy</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">API</a>
+                <a href="#">API</a>
               </li>
               </ul>
             </div>
-            <div className="col-3">
+            <div id="contacts-this" className="col-2">
               <h4 id="contacts">Contacts</h4>
               <ul>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">+62 812 3568 6798</a>
+                <a href="#">+62 812 3568 6798</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">speakup@gmail.com</a>
+                <a href="#">speakup@gmail.com</a>
               </li>
               </ul>
             </div>
